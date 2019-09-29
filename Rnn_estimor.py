654555452bb656762c41612aba6efdda8efa3384
YassineZAIBI <44236203+YassineZAIBI@users.noreply.github.com>
2019-09-29 15:50:50 +0200

    Add files via upload
    An RNN network estimator developed under Tensorflow 2.0 beta

--- Rnn_estimor.py
+++ Rnn_estimor.py
@@ -9,13 +9,16 @@ import tensorflow as tf
 
 
 class Rnn_Estimator(tf.keras.Model):
-    def __init__(self,units1):
+    def __init__(self, units1):
         super(Rnn_Estimator).__init()
-        self.initializer1 = tf.keras.initializers.lecun_normal(seed = 73, epoch)
-        self.units11 = units1 
-        self.lstm_layer1 = tf.keras.layers.LSTM(units = self.units1 , return_sequences=True, kernel_initializer = self.initializer1)
-        self.lstm_dropout1  = tf.keras.layers.Dropout(rate = 0.2)
-        self.lstm_dense1 = tf.keras.layers.Dense(units = 1, activation = "sigmoid" )
+        self.initializer1 = tf.keras.initializers.lecun_normal(seed=73, epoch)
+        self.units11 = units1
+        self.lstm_layer1 = tf.keras.layers.LSTM(units=self.units1,
+                                                return_sequences=True,
+                                                kernel_initializer=self.initializer1)
+        self.lstm_dropout1  = tf.keras.layers.Dropout(rate=0.2)
+        self.lstm_dense1 = tf.keras.layers.Dense(units=1,
+                                                 activation="sigmoid")
         self.loss_object = tf.keras.losses.MeanSquaredError()
         self.optimizer = tf.keras.optimizers.Adam()
         self.train_loss = tf.keras.metrics.Mean(name='train_loss')
@@ -27,7 +30,7 @@ class Rnn_Estimator(tf.keras.Model):
         
         def call(self):
             x = inputs
-            x = tf.keras.Input(shape = [batch , timestemp, features])
+            x = tf.keras.Input(shape=[batch, timestemp, features])
             x = self.lstm_layer1(x)
             x = self.lstm_dropout1(x)
             return self.lstm_dense1(x)
@@ -37,11 +40,12 @@ class Rnn_Estimator(tf.keras.Model):
                 predictions = self.call(x_train)
                 loss = self.loss_object(y_train, predictions)
             gradients = tape.gradient(loss, self.trainable_variables)
-            self.optimizer.apply_gradients(list(zip(gradients, self.trainable_variables)))
+            self.optimizer.apply_gradients(list(zip(gradients,
+                                                    self.trainable_variables)))
             self.train_loss(loss)
             self.train_accuracy(y_train, predictions)
             
-        def test_step(self, x_test , y_test): 
+        def test_step(self, x_test , y_test):
             predictions = self.call(x_test)
             t_loss = self.loss_object(y_test, predictions)
             
@@ -61,8 +65,12 @@ class Rnn_Estimator(tf.keras.Model):
                 self.train_step(x_train,y_train)
                 self.test_step(x_test, y_test)
                 with self.train_summary_writer.as_default():
-                    self.summary_loss = tf.summary.scalar('loss',self.test_loss.result(),step = epoch)
-                    self.summary_accuarcy = tf.summary.scalar('accuracy', self.test_accuracy.result(), step = epoch)
+                    self.summary_loss = tf.summary.scalar('loss',
+                                                          self.test_loss.result(),
+                                                          step=epoch)
+                    self.summary_accuarcy = tf.summary.scalar('accuracy',
+                                                              self.test_accuracy.result(),
+                                                              step=epoch)
                     
 if __name__ == "__main__":  
     my_model = Rnn_Estimator()
